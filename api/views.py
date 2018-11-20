9ab3facf4eafbc78eb8feb3b4937b6a9ab78e098
khm56 <43205666+khm56@users.noreply.github.com>
2018-11-20 12:40:13 +0300

    Update views.py

--- api/views.py
+++ api/views.py
@@ -11,12 +11,40 @@ from .serializers import UserCreateSerializer, ItemListViewSerializer, DetailVie
 class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
 
-class ListAPIView(RetrieveAPIView):
-    serializer_class = ItemListViewSerializer
+class ItemListAPIView(RetrieveAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemListSerializer
+    # permission_classes = [AllowAny,]
+    # filter_backends = [OrderingFilter, SearchFilter,]
+    # search_fields = ['name', 'description', 'owner__username']
 
 class DetailAPIView(RetrieveAPIView):
-    serializer_class = DetailViewSerializer
+    queryset = Item.objects.all()
+    serializer_class = ItemDetailSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'item_id'
+    # permission_classes = [AllowAny,]
+
+class ItemCreateAPIView(CreateAPIView):
+    serializer_class = ItemCreateUpdateSerializer
+    # permission_classes = [IsAuthenticated,]
+
+    # def perform_create(self, serializer):
+    #     serializer.save(owner=self.request.user)
+
+class ItemUpdateAPIView(RetrieveUpdateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemCreateUpdateSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'item_id'
+    # permission_classes = [IsAuthenticated,IsOwner]
+
+class ItemDeleteAPIView(DestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemListSerializer
+    lookup_field = 'id'
+    lookup_url_kwarg = 'item_id'
+    # permission_classes = [IsAuthenticated,IsAdminUser]
 
 class OrderAPIView(RetrieveAPIView):
     serializer_class = OrderViewSerializer
-
